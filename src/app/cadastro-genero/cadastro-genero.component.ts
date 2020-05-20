617e5734b588a2c278152641bd4cd0e346cf49fc
Matheus Stimer <mtstimer@gmail.com>
2020-05-20 16:08:00 -0300

    a

--- src/app/cadastro-genero/cadastro-genero.component.ts
+++ src/app/cadastro-genero/cadastro-genero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
+import { Genero } from '../models/genero.model';
+import { GenerosService } from '../services/generos.service';
 
 @Component({
   selector: 'app-cadastro-genero',
@@ -14,12 +16,27 @@ export class CadastroGeneroComponent implements OnInit {
 
   @ViewChild(FormGroupDirective) formGroupDirective: FormGroupDirective;
   
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private generosService: GenerosService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  submit(){
+  async submit() {
+
+    if (!this.formulario.valid) {
+      return;
+    }
+
+    this.formulario.disable();
+
+    const genero = this.formulario.value as Genero;
+    const generoRetorno = await this.generosService.add(genero);
+
+    this.formulario.enable();
+    this.formGroupDirective.resetForm();
 
   }
 }
